469b5f8283a12c02447d9d13d1f6669076c51a5f
AtharvaKajwe <atharva.kajwe22@spit.ac.in>
2025-12-18 03:26:18 +0530

    Fixing code which gave errors while deploying2

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -39,7 +39,6 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
         </div>
       </div>
       <div className="flex items-center gap-3">
-        {/* {getWeatherIcon()} */}
         <button
           onClick={onThemeToggle}
           className="p-2 rounded-lg bg-white/20 hover:bg-white/30 transition-colors"
